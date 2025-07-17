bbc87cd75da8b2bb638e9759fb14572ac0e1d296
Armin Berger <armin@bergr.ch>
2025-07-17 10:02:53 +0200

    update wandb name

--- src/train_classifier/run_classification.py
+++ src/train_classifier/run_classification.py
@@ -112,7 +112,7 @@ def run_classification(
             "post_window should always be 0 since it is not supported anymore"
         )
 
-    wandb_run = wandb.init(project=f'Classifier Training vFINAL', config={
+    wandb_run = wandb.init(project=f'Classifier Training vFINAL clip true', config={
         'feature_extractor_name': feature_extractor_name,
         'classifier_name': classifier_name,
         'dataset_name': dataset_name,
