b068079225154eca6460acd3b1e3649b6682e6f3
Armin Berger <armin@bergr.ch>
2025-07-26 14:27:27 +0200

    add script for matrix computation

--- eval_scripts/aug_combinations.sh
+++ eval_scripts/aug_combinations.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 augmentations=("na" "shuffle" "jit_scal" "perm_jit" "resample" "noise" "scale" "negate" "t_flip" "rotation" "hfc" "lfc" "p_shift" "ap_p" "ap_f" "perm_harnet" "t_warp_harnet")
 
 combinations=()
@@ -9,7 +11,6 @@ for ((i = 0; i < ${#augmentations[@]}; i++)); do
   done
 done
 
-# Example usage: print them
 for combo in "${combinations[@]}"; do
-   uv run main.py task='train_ssl' feature_extractor.augmentations="$combo"
+   sbatch --time=24:00:00 --gpus-per-node=1 --cpus-per-task=1 --mem-per-cpu=32G --wrap="uv run main.py task='train_ssl' feature_extractor.augmentations=\"\$combo\" feature_extractor/ml_config=matrix_comp"
 done
