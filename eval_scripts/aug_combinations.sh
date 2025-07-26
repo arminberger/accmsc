addb47418608bbcfb47045915eef14df35207c52
Armin Berger <armin@bergr.ch>
2025-07-26 19:25:33 +0200

    add script for matrix computation

--- eval_scripts/aug_combinations.sh
+++ eval_scripts/aug_combinations.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-augmentations=("na" "shuffle" "jit_scal" "perm_jit" "resample" "noise" "scale" "negate" "t_flip" "rotation" "hfc" "lfc" "p_shift" "ap_p" "ap_f" "perm_harnet" "t_warp_harnet")
+augmentations=("na" "shuffle" "jit_scal" "perm_jit" "resample" "noise" "scale" "negate" \
+               "t_flip" "rotation" "hfc" "lfc" "p_shift" "ap_p" "ap_f" "perm_harnet" "t_warp_harnet")
 
 combinations=()
 
-for ((i = 0; i < ${#augmentations[@]}; i++)); do
-  for ((j = i + 1; j < ${#augmentations[@]}; j++)); do
+# build all 2-element subsets
+for ((i=0; i < ${#augmentations[@]}; i++)); do
+  for ((j=i+1; j < ${#augmentations[@]}; j++)); do
     combo="['${augmentations[i]}', '${augmentations[j]}']"
     combinations+=("$combo")
   done
 done
 
+# submit a job for each subset
 for combo in "${combinations[@]}"; do
-   sbatch --time=24:00:00 --gpus-per-node=1 --cpus-per-task=1 --mem-per-cpu=32G --wrap="uv run main.py task='train_ssl' feature_extractor.augmentations=\"\$combo\" feature_extractor/ml_config=matrix_comp"
+  echo sbatch --time=24:00:00 --gpus-per-node=1 --cpus-per-task=1 --mem-per-cpu=32G --wrap="uv run main.py task='train_ssl' feature_extractor.augmentations=\"${combo}\" feature_extractor/ml_config=matrix_comp"
+  sbatch --time=24:00:00 --gpus-per-node=1 --cpus-per-task=1 --mem-per-cpu=32G --wrap="uv run main.py task='train_ssl' feature_extractor.augmentations=\"${combo}\" feature_extractor/ml_config=matrix_comp"
 done
