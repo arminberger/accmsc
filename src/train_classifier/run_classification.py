7586b6338f1fc1ab6b726bbcd41425313b03bd23
Armin Berger <armin@bergr.ch>
2025-10-07 17:26:16 +0200

    change wandb name

--- src/train_classifier/run_classification.py
+++ src/train_classifier/run_classification.py
@@ -125,7 +125,7 @@ def run_classification(
             "post_window should always be 0 since it is not supported anymore"
         )
 
-    wandb_run = wandb.init(project=f'Charite With Heart Rate', config={
+    wandb_run = wandb.init(project=f'Charite HR Std Ablation Study', config={
         'feature_extractor_name': feature_extractor_name,
         'classifier_name': classifier_name,
         'dataset_name': dataset_name,
